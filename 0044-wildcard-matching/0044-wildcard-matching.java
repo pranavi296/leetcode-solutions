2a6f0a93977b2978d9ae69aab0ebaeebad47aafd
pranavi296 <pranavikaad@gmail.com>
2026-09-07 21:32:19 +0530

    Time: 2 ms (93.24%), Space: 45.6 MB (72.35%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int i = 0, j = 0;
+        int starIndex = -1;
+        int match = 0;
+
+        while (i < s.length()) {
+            if (j < p.length() &&
+                (p.charAt(j) == s.charAt(i) || p.charAt(j) == '?')) {
+                i++;
+                j++;
+            }
+            else if (j < p.length() && p.charAt(j) == '*') {
+                starIndex = j;
+                match = i;
+                j++;
+            }
+            else if (starIndex != -1) {
+                j = starIndex + 1;
+                match++;
+                i = match;
+            }
+            else {
+                return false;
+            }
+        }
+        while (j < p.length() && p.charAt(j) == '*') {
+            j++;
+        }
+
+        return j == p.length();
+    }
+}
